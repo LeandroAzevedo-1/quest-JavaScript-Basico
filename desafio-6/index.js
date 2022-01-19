a1dcafbca50dc804c44f5bf9d281d52ec99d908f
perkles <leoazevedo1450@gmail.com>
2022-01-19 16:07:53 -0300

    Código

--- desafio-6/index.js
+++ desafio-6/index.js
@@ -1,9 +1,31 @@
+//Função Soma 
 function somar(numero1,numero2){
     const soma = numero1 + numero2;
-    return ( "A soma dos dois números e: " + soma)
+    return ( "A soma dos dois números é: " + soma)
 }
-soma = somar(1 , 3)
+soma = somar(10 , 3)
 console.log(soma)
 
-soma = somar(10 ,10)
-console.log(soma)
+//função subtração
+function subtracao(numero1, numero2){
+    const subtrair = numero1 - numero2;
+    return ( "A subtração dos dois números é: " + subtrair)
+}
+subtrair = subtracao(10, 3)
+console.log(subtrair)
+
+//função divisao 
+function divisao(numero1, numero2) {
+    let dividir = numero1 / numero2;
+    return ('A divisão dos dois números é: ' + dividir);
+}
+dividir = divisao(10 , 2)
+console.log(dividir)
+
+//função multiplicação
+function multipicacao(numero1, numero2) {
+    let multiplicar = numero1 * numero2;
+    return ('A multiplicação dos dois números é: ' + multiplicar);
+}
+multipicar = multipicacao(10, 2)
+console.log(multipicar)
